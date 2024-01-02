c2999ea6f90a4c2086aef071c033929af1de4ef7
bewithsnehash <mandalsneh97@gmail.com>
2024-01-03 00:59:50 +0530

    Todo App Frontend using DOM

--- Week 3/Week 3.1/TO-DO App with API/todofile.js
+++ Week 3/Week 3.1/TO-DO App with API/todofile.js
@@ -4,7 +4,10 @@ const fs = require("fs");
 // const port = 3000;
 const path = require("path");
 const app = express();
+const cors = require('cors');
+    
 
+app.use(cors());
 app.use(bodyParser.json());
 
 function findIndex(arr, id) {
@@ -103,13 +106,17 @@ app.get("/",(req,res)=>{
   res.sendFile(path.join(__dirname,"index.html"))
 })
 
+app.post("/",(req,res)=>{
+  res.readFile(path.join(__dirname,"index.html"))
+})
+
 // for all other routes, return 404
 // app.use((req, res, next) => {
 //   res.status(404).send();
 // });
 
 
-app.listen(3000);
+app.listen(3001);
 // function started(){
 //   console.log(`Example app listening on port ${port}`);
 // }
