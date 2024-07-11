2e5d86d1d14bdef38a0fa665c305e88c7fdb0cb3
bewithsnehasish <mandalsneh97@gmail.com>
2024-07-11 18:39:29 +0530

    Learnt Week 6 Concepts of Wrapper Class

--- Week-06/Week 6.1/Week 6.1 React WrapperClasses/src/App.jsx
+++ Week-06/Week 6.1/Week 6.1 React WrapperClasses/src/App.jsx
@@ -1,23 +1,26 @@
-import React from "react"
+
+//This is a way to declare the wrapper classes in the React 
 function App() {
   return <div>
-    <CardWrapper innerComponent={<TextComponent />} />
+    <CardWrapper>
+      hi there
+    </CardWrapper>
   </div>
 }
 
 
-function CardWrapper({ innerComponent }) {
+function CardWrapper({ children }) {
   return (
     <div style={{ border: "2px solid black", padding: '10px' }}>
-      {innerComponent}
+      {children}
     </div>
   );
 }
 
-function TextComponent() {
-  return <div>
-    hi there
-  </div>
-}
+// function TextComponent() {
+//   return <div>
+//     hi there
+//   </div>
+// }
 
 export default App
