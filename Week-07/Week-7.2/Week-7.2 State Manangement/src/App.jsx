5fe6f7100ee4e693cdd4d71ff552f582d62dcc7d
bewithsnehasish <mandalsneh97@gmail.com>
2024-07-17 01:48:18 +0530

    Learnt about Selectors Week 7.2

---
+++ Week-07/Week-7.2/Week-7.2 State Manangement/src/App.jsx
@@ -0,0 +1,99 @@
+import {
+  RecoilRoot,
+  useRecoilState,
+  useRecoilValue,
+  useSetRecoilState,
+} from "recoil";
+import { countAtom } from "./store/atoms/count";
+import { evenSelector } from "./store/selectors/selector";
+
+function App() {
+  return (
+    <div>
+      <RecoilRoot>
+        <Count />
+      </RecoilRoot>
+    </div>
+  );
+}
+
+function Count() {
+  return (
+    <div>
+      <CountRenderer />
+      <Buttons />
+    </div>
+  );
+}
+
+function CountRenderer() {
+  const count = useRecoilValue(countAtom);
+  return (
+    <div>
+      {count}
+      <EvenCountRenderer />
+    </div>
+  );
+}
+
+function EvenCountRenderer() {
+  const isEven = useRecoilValue(evenSelector);
+  return <div>{isEven ? "It is Odd" : "It is a Even number"}</div>;
+}
+
+// //Function made using the state component but the buttons also re-renders so to avoid it we can do this
+
+// function Buttons() {
+//   //useRecoilState brings the component as the staate component
+// const [count, setCount] = useRecoilState(countAtom);
+//   return (
+//     <div>
+//       <button
+//         type="button"
+//         onClick={() => {
+//           setCount(count + 1);
+//         }}
+//       >
+//         Increase Count
+//       </button>
+
+//       <button
+//         type="button"
+//         onClick={() => {
+//           setCount(count - 1);
+//         }}
+//       >
+//         Decrease Count
+//       </button>
+//     </div>
+//   );
+// }
+
+function Buttons() {
+  const setCount = useSetRecoilState(countAtom);
+  console.log("buttons re-renders");
+
+  return (
+    <div>
+      <button
+        type="button"
+        onClick={() => {
+          setCount((count) => count + 1);
+        }}
+      >
+        Increase
+      </button>
+
+      <button
+        type="button"
+        onClick={() => {
+          setCount((count) => count - 1);
+        }}
+      >
+        Decrease
+      </button>
+    </div>
+  );
+}
+
+export default App;
