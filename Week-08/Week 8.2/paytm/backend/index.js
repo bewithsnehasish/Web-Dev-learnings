f056ec65fab1d7fb8b962d296ae3e7c87f7f71ea
bewithsnehasish <mandalsneh97@gmail.com>
2024-07-31 20:31:55 +0530

    Added Routes

--- Week-08/Week 8.2/paytm/backend/index.js
+++ Week-08/Week 8.2/paytm/backend/index.js
@@ -2,8 +2,9 @@ const express = require("express");
 const cors = require("cors");
 
 const mainRouter = require("./routes/index");
-
 const app = express();
 
-app.use("/api/v1", mainRouter);
 app.use(cors());
+app.use(express.json());
+app.use("/api/v1", mainRouter);
+app.listen(3000);
--- Week-08/Week 8.2/paytm/backend/routes/index.js
+++ Week-08/Week 8.2/paytm/backend/routes/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
-const userRouter = require("./routes/user");
+const userRouter = require("./user");
+
+const app = express();
 
 const router = express.Router();
 
 module.exports = router;
-app.use("/api/v1/user", userRouter);
+app.use("/user", userRouter);
