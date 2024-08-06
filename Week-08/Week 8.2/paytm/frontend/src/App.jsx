0c5a9550e64de8dfadcedcb3f08951079369d76f
bewithsnehasish <mandalsneh97@gmail.com>
2024-08-07 00:23:00 +0530

    Components and Sign in & Sign Up page

--- Week-08/Week 8.2/paytm/frontend/src/App.jsx
+++ Week-08/Week 8.2/paytm/frontend/src/App.jsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Signup } from "./pages/Signup";
+import { Signin } from "./pages/Signin";
 
 function App() {
   return (
@@ -6,8 +8,9 @@ function App() {
       <Routes>
         <Route path="/signup" element={<Signup />} />
         <Route path="/signin" element={<Signin />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        {/*<Route path="/dashboard" element={<Dashboard />} />
         <Route path="/send" element={<SendMoney />} />
+        */}
       </Routes>
     </BrowserRouter>
   );
--- Week-08/Week 8.2/paytm/frontend/src/components/InputBox.jsx
+++ Week-08/Week 8.2/paytm/frontend/src/components/InputBox.jsx
@@ -1,8 +1,12 @@
-export function InputBox ({placeholder, label}){
-  return <div>
-    <div className="text-sm font-medium text-left py-2">
-      {label}
+export function InputBox({ placeholder, label }) {
+  return (
+    <div>
+      <div className="text-sm font-medium text-left py-2">{label}</div>
+      <input
+        type="text"
+        placeholder={placeholder}
+        className="w-full px-2 py-1 border rounded border-slate-200"
+      />
     </div>
-    <input type="text" placeholder={placeholder} className="w-full px-2 py-1 border rounded border-slate-200">
-  </div>
+  );
 }
