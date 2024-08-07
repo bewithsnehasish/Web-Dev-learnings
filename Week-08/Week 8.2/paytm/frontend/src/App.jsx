079527b4ee487591aea5ba5024ed25ae61876a1c
bewithsnehasish <mandalsneh97@gmail.com>
2024-08-07 23:46:51 +0530

    SendMoney Page

--- Week-08/Week 8.2/paytm/frontend/src/App.jsx
+++ Week-08/Week 8.2/paytm/frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Signup } from "./pages/Signup";
 import { Signin } from "./pages/Signin";
+import { SendMoney } from "./pages/SendMoney";
 
 function App() {
   return (
@@ -8,9 +9,9 @@ function App() {
       <Routes>
         <Route path="/signup" element={<Signup />} />
         <Route path="/signin" element={<Signin />} />
-        {/*<Route path="/dashboard" element={<Dashboard />} />
         <Route path="/send" element={<SendMoney />} />
-        */}
+        {/*<Route path="/dashboard" element={<Dashboard />} />
+         */}
       </Routes>
     </BrowserRouter>
   );
